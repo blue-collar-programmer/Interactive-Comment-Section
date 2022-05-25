3de7f74571a9b074ae4322f69995aa98e01fed03
blue-collar-programmer <89601260+blue-collar-programmer@users.noreply.github.com>
2022-05-25 14:42:48 -0700

    Finally got the voting feature to work and rightly subtract and addd the vote of user only once per direction. One vote up and one vote down. Also stored all of it in the local storage so the the last state of the users actions are still displayed on the page when they refresh or reload the page

--- comments.js
+++ comments.js
@@ -5,27 +5,27 @@ const commentTextArea = document.getElementById('commentTextArea');
 let clickCount = 0;
 
 // Comment area eventListener
-commentTextArea.addEventListener('click', (e)=>{
+commentTextArea.addEventListener('click', (e) => {
     clickCount++
     console.log('this is the eventTarget=>', e)
-    if (clickCount % 2 === 0){
+    if (clickCount % 2 === 0) {
         unActiveBorderStyles(e.target)
     } else {
-        activeBorderStyles(e.target)    
+        activeBorderStyles(e.target)
     }
 
 })
 //gives a border shadow to the textarea
-let activeBorderStyles = (el)=>{
+let activeBorderStyles = (el) => {
     let textArea = el;
     return textArea.style.cssText = "box-shadow: 0px 0px 8px  hsl(238, 40%, 52%);"
-    
+
 }
 //removes border shadow from the textarea
-let unActiveBorderStyles = (el)=>{
+let unActiveBorderStyles = (el) => {
     let textArea = el;
     return textArea.style.cssText = "box-shadow: none;"
-    
+
 }
 
 
@@ -44,12 +44,12 @@ async function getCommentsData(url) {
 getCommentsData('data.json')
     .then((data) => {
 
-//  Tracking the original state of the score property value by saving to localStorage, then updating by clearing
-/*      But why did I initiate this outside of the loop?
-        So that there would be only one iteration and not two
-        -By placing this inside of the forEach loop, the local storage would simply be replaced with the current comment objects score
-        -This means I would only have access to the last rendered objects and the rest would not have survived the last iteration
-*/  
+        //  Tracking the original state of the score property value by saving to localStorage, then updating by clearing
+        /*      But why did I initiate this outside of the loop?
+                So that there would be only one iteration and not two
+                -By placing this inside of the forEach loop, the local storage would simply be replaced with the current comment objects score
+                -This means I would only have access to the last rendered objects and the rest would not have survived the last iteration
+        */
         let origScore = [];
         for (let i = 0; i < data.comments.length; i++) {
             if (i != origScore[i]) {
@@ -59,15 +59,15 @@ getCommentsData('data.json')
             }
         }
         console.log('Final score test=>', origScore)
-        currentUsersAvatar.src=`${data.currentUser.image.png}`
+        currentUsersAvatar.src = `${data.currentUser.image.png}`
         // first render the comments properties
         // next add a .then and render the replies, assigning their on container/ class etc. to them
-        
-// Initiated the data obj and the comment properties to a const variable named, "comments"
+
+        // Initiated the data obj and the comment properties to a const variable named, "comments"
         const comments = data.comments;
         console.log('Testing each comment object=> ', comments);
 
-        comments.forEach((row, i )=> {
+        comments.forEach((row, i) => {
 
             const commentContainer = document.createElement('div');
             allCommentsContainer.appendChild(commentContainer);
@@ -115,7 +115,7 @@ getCommentsData('data.json')
             <img class= "replyIcon" src = "../images/icon-reply.svg"> Reply
             `
             userActionsFlexContainer.appendChild(replyButton);
-            
+
             voteCount.innerHTML = `
             ${localStorage.getItem(origScore[i])}
             `
@@ -126,68 +126,90 @@ getCommentsData('data.json')
             minusButton.innerHTML = `
             <img class = "votingIcons minus" src="../images/icon-minus.svg">
             `
-/* A new global scoped variable (in the context of this api call) to keep track of each users vote
-        Can be used and accessed by both event handlers to: create, read , update, and delete the last value in localStorage          
-
-*/            
-           
 
             plusButton.addEventListener('click', (e) => {
 
-                console.log('origScore =>',Number(localStorage.getItem(origScore[i])) + 1)
 
                 let plusClicks = 0;
                 plusClicks++
-                let newVoteCount = Number(localStorage.getItem(origScore[i])) + plusClicks;
-                localStorage.setItem('newVoteCount', `${newVoteCount}`);
+                let newVoteCount;
 
-                if(localStorage.getItem('newVoteCount') === 0 || localStorage.getItem('newVoteCount') === null || localStorage.getItem('newVoteCount') === undefined){
-                    console.log('FIRED ONE!');
-                    voteCount.innerHTML= `
-                    ${localStorage.getItem(origScore[i])}
+                if (localStorage.getItem('newVoteCount') < localStorage.getItem(origScore[i]) || localStorage.getItem('newVoteCount') === undefined) {
+console.log('fired')
+
+                    newVoteCount = Number(localStorage.getItem(origScore[i]));
+                    localStorage.setItem('newVoteCount', newVoteCount);
+                    
+                    voteCount.innerHTML = `
+                    ${Number(localStorage.getItem('newVoteCount'))}
+                    `
+
+
+                } else if (Number(localStorage.getItem('newVoteCount')) === Number(localStorage.getItem(origScore[i]))) {
+                    console.log('fired2')
+                    
+                    newVoteCount = Number(localStorage.getItem(origScore[i])) + plusClicks;
+                    localStorage.setItem('newVoteCount', newVoteCount)
+                    
+                    voteCount.innerHTML = `
+                    
+                    ${Number(localStorage.getItem('newVoteCount'))}
                     
                     `
-                } else {
-                    console.log('FIRED TWO!=>')
-                    voteCount.innerHTML= `
-                    ${localStorage.getItem('newVoteCount')}
-                    `
+
                 }
 
-                if( Number(localStorage.getItem('newVoteCount')) > Number(localStorage.getItem(origScore[i])) + 1){
-                        console.log('FIRED THREE!')
-                        voteCount.innerHTML= `
-                        ${Number(localStorage.getItem('newVoteCount')) - 1}
-                        
-                       ` 
-                }
-                    
-                
+
                 /*if ( clickCount >= 1) {
                     alert('you can only give one upvote per user')
                 } */
-                
+
                 plusClicks = 0;
             })
 
             minusButton.addEventListener('click', (e) => {
                 let minusClicks = 0;
                 minusClicks++
-                if ( /*localStorage.getItem('plusCount')*/ clickCount >= 1) {
-                    alert('you can only give one downvote per user')
+                //localStorage.setItem('minusClicks', minusClicks)
+
+                let newVoteCount;
+
+                if (localStorage.getItem('newVoteCount') > localStorage.getItem(origScore[i]) || localStorage.getItem('newVoteCount') === undefined) {
+                    
+                    newVoteCount = Number(localStorage.getItem(origScore[i])) ;
+                    localStorage.setItem('newVoteCount', newVoteCount)
+                    
+                    voteCount.innerHTML = `
+                
+                ${Number(localStorage.getItem('newVoteCount'))}
+
+                `
+
+                } else if (Number(localStorage.getItem('newVoteCount')) === Number(localStorage.getItem(origScore[i]))) {
+                    
+                    newVoteCount = Number(localStorage.getItem(origScore[i])) - minusClicks;
+                    localStorage.setItem('newVoteCount', newVoteCount);
+                    
+                    voteCount.innerHTML = `
+
+                    ${Number(localStorage.getItem('newVoteCount'))}
+
+                    `
                 }
-
+                // localStorage.removeItem('minusClicks');
                 minusClicks = 0;
-       })
-            
+            })
 
+            window.location.refresh = function() {
+                console.log('LOAD EVENT FIRED ')
+                voteCount.innerHTML = `
+                ${localStorage.getItem('newVoteCount')}
+                `
+            }
         })
 
     })
-    
+
 
 // add clickCount as the parameter to a function whos job is to increase the displayed vote count:
 // - then saving it to the local storage ,and displaying it to in the innerHTML = '';
-
-
-    
